76c658d992d36394bcdbbdcd6f2030fbe68b4595
MartinasR <martinasrudis@gmail.com>
2022-07-08 10:38:06 +0300

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Navigation from "./components/Navigation/navigation";
-import Register from "./components/Register/register";
-import Login from "./components/Login/login";
-import Home from "./components/Home/home";
-import Add from "./components/Add/add";
-import LoggedIn from "./components/Home/loggedInn";
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
 
 function App() {
   return (
     <>
-      <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<LoggedIn />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="*" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
     </>
   );
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <h2 >Copyright © Martinas R.</h2>
+        </footer>
+    );
+}
+ 
+export default Footer;
