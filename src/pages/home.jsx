62d5adb20efb4596d280233f621632e8671d9f98
MartinasR <martinasrudis@gmail.com>
2022-07-08 13:03:29 +0300

    card update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import Menu from '../components/menu/menu'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 
-import SkillCard from '../components/SkillCard/skillCard';
+import SkillCard from '../components/card1/card';
 import Footer from '../components/footer/footer';
 
 
