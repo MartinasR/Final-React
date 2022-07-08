d59e1239d5fa418d0cc8ed30c9860ea1a00a58dc
MartinasR <martinasrudis@gmail.com>
2022-07-08 10:07:57 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation/navigation";
+import Register from "./components/Register/register";
+import Login from "./components/Login/login";
+import Home from "./components/Home/home";
+import Add from "./components/Add/add";
+import LoggedIn from "./components/Home/loggedInn";
 
 function App() {
   return (
     <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<LoggedIn />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
   );
 }
